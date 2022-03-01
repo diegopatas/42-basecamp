e6725f3dca58c9870c26a06263bc831cceceecd3
Diego Diniz <oitopatas.email@gmail.com>
2022-02-28 21:40:54 -0300

    Update repo.

---
+++ 0_lista_c_00/0002_ft_print_reverse_alphabet.c
@@ -0,0 +1,36 @@
+// ID: 
+// Tag: [[clang]] [[basecamp]]
+// Type: #type/tech
+// Status: #status/sown 
+// Cntxt: Basecamp da 42 variação dos exercícios - lista c00
+// Prvs: 
+// Nxt: 
+// Brnch: 
+//
+// #### ex02
+// 
+// ---
+// 
+// ```c
+
+#include <unistd.h>
+
+void    ft_print_reverse_alphabet(void)
+{
+    char    ch;
+
+    ch = 'z';
+    while (ch >= 'a')
+    {
+        write(1, &ch, 1);
+        ch--;
+    }
+}
+
+// int main(void)
+// {
+//     ft_print_reverse_alphabet();
+//     return (0);
+// }
+
+// ```
---
+++ 0_lista_c_00/0003_ft_print_numbers.c
@@ -0,0 +1,34 @@
+// ID: 
+// Tag: [[clang]] [[basecamp]]
+// Type: #type/tech
+// Status: #status/sown 
+// Cntxt: Basecamp da 42 variação dos exercícios - Lista c00
+// Prvs: 
+// Nxt: 
+// Brnch: 
+//
+// #### ex03
+// ---
+// 
+// ```c
+
+void    ft_print_numbers(void)
+{
+    int numb;
+    char    ch;
+    
+    numb = 0;
+    while (numb <= 9)
+    {
+        ch = numb + '0';
+        write(1, &ch, 1);
+    }
+}
+
+// int main(void)
+// {
+//     ft_print_numbers();
+//     return (0);
+// }
+// 
+// ```
